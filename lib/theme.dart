218c9381b900011711f453a97e8f7c75aa6fbeec
Abu Anwar <abuanwar072@gmail.com>
2021-02-18 15:39:19 +0600

    starting

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,13 +1,13 @@
 import 'package:chat/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData lightThemeData(BuildContext context) {
   return ThemeData.light().copyWith(
     primaryColor: kPrimaryColor,
     scaffoldBackgroundColor: Colors.white,
     iconTheme: IconThemeData(color: kContentColorLightTheme),
-    textTheme: Theme.of(context)
-        .textTheme
+    textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme)
         .apply(displayColor: kContentColorLightTheme),
     colorScheme: ColorScheme.light(
       secondary: kSecondaryColor,
@@ -20,7 +20,7 @@ ThemeData darkThemeData(BuildContext context) {
   return ThemeData.dark().copyWith(
     scaffoldBackgroundColor: kContentColorLightTheme,
     iconTheme: IconThemeData(color: kContentColorDarkTheme),
-    textTheme:
-        Theme.of(context).textTheme.apply(displayColor: kContentColorDarkTheme),
+    textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme)
+        .apply(displayColor: kContentColorDarkTheme),
   );
 }
